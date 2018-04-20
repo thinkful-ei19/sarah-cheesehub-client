d01c06fd132b0709bf83b5d799c14e2a25a39a32
ssheaks <sarah.sheaks@gmail.com>
2018-04-20 11:32:36 -0700

    update cheese-list

--- src/components/cheese-list.js
+++ src/components/cheese-list.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import { fetchCheeses } from '../actions/cheeseAct';
 //import {some action} from '../actions/action'
 
 class CheeseList extends React.Component {
   componentDidMount(){
     console.log(this.props)
-    //this.props.dispatch(some WebGLActiveInfo())
+    this.props.dispatch(fetchCheeses())
   }
-}
 
-render(){
+
+render() {
+
   console.log(this.props);
+  const cheeseList = this.props.cheeses.map((cheese, index)=>{
+    return(
+        <li key={index}> {cheese} </li>
+    )
+})
   return(
     <div className="cheeseList">
       <ul>
-        <li>cheese</li>
+        { cheeseList }
       </ul>
     </div>
   )
 }
+}
 
+const mapStateToProps = (state) => ({
+  cheeses: state.cheeses
+})
+
+export default connect(mapStateToProps)(CheeseList);
