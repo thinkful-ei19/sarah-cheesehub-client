6d93d7c19415e76bccf296ef1d5c8d5f488e8381
ssheaks <sarah.sheaks@gmail.com>
2018-04-20 10:09:12 -0700

    create config.js file

---
+++ src/reducers/cheeseReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    cheeses: [],
+    loading: false,
+    error: null
+}
+
+const cheeseReducer = (state =  initialState, action) => {
+  return state
+}
+
+export default cheeseReducer
